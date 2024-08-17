74801ebe2681c4c585c71f8e02ed76fcc4b9d8ca
Dmitrii Romanov <RomDim197@gmail.com>
2024-08-17 20:08:11 +0300

    menu added

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
   });
 
   // Управление попапами
-
+  // попап отправить контакт
   $('.pagecrm_prices__btn').on('click', function () {
     $('.pagecrm_popup').addClass('pagecrm_popup_opened');
   });
@@ -25,9 +25,23 @@ $(document).ready(function () {
   // Скрываем popup при клике вне popup
   $(document).on('click', function (event) {
     let $target = $(event.target);
-
+    // для попап заказ звонка
     if (!$target.closest('.pagecrm_popup__form').length && !$target.is('.pagecrm_prices__btn')) {
       $('.pagecrm_popup').removeClass('pagecrm_popup_opened');
     }
+    // для попап меню
+    if (!$target.closest('.pagecrm_nav-block__burger, #popup_menu').length) {
+      $('#popup_menu').removeClass('show'); // Скрыть меню
+    }
+  });
+  // попап бургер
+  $('.pagecrm_nav-block__burger').on('click', function (e) {
+    e.stopPropagation(); // Остановить распространение события клика
+    $('#popup_menu').toggleClass('show'); // Показать или скрыть меню
+  });
+
+  // Остановка распространения события клика на элементы внутри меню
+  $('#popup_menu').on('click', function (e) {
+    e.stopPropagation(); // Остановить распространение события клика внутри меню
   });
 });
